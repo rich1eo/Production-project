44cf8a6666789b33c52834cc665a7e965e003639
Vladislav Rychkov <v.richkov20@gmail.com>
2023-09-05 11:07:57 +0300

    Add Not found Page, Loader

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,17 @@
+import './Loader.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+interface LoaderProps {
+  className?: string;
+}
+
+export default function Loader({ className }: LoaderProps) {
+  return (
+    <div className={classNames('lds-ellipsis', {}, [className])}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
---
+++ src/widgets/PageLoader/PageLoader.tsx
@@ -0,0 +1,15 @@
+import styles from './PageLoader.module.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import Loader from 'shared/ui/Loader/Loader';
+
+interface PageLoaderProps {
+  className?: string;
+}
+
+export default function PageLoader({ className }: PageLoaderProps) {
+  return (
+    <div className={classNames(styles.PageLoader, {}, [className])}>
+      <Loader />
+    </div>
+  );
+}
